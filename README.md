4b036d6f640d3d5f65507218e15fc5b538be87f6
Néstor Coppi <nestorcoppi@gmail.com>
2012-07-29 03:01:17 -0300

    Add two helpfull methods =)

--- README.md
+++ README.md
@@ -18,30 +18,6 @@ Add in the models that we want to use
 
     include Fortnight
 
-## Usage
-
-It's very simple, the syntax is the next:
-
-    Model.in_fortnight(year, month, options)
-
-'options' for now are only :field and :half
-    :field ALWAYS HAVE TO BE A TIME FIELD
-    :half is to get the first (by default) or the second (as symbol)
-
-## Examples
-
-    Customer.in_fortnight(2012, 04)
-We'll get the customers created_at between 2012-04-01 00:00 AND 2012-04-15 23:59:59
-
-    Customer.in_fortnight(2012, 04, half: :second)
-We'll get the customers created_at between 2012-04-15 23:59:59 AND 2012-04-30 23:59:59
-
-    Customer.in_fortnight(2012, 05, half: :second)
-We'll get the customers created_at between 2012-05-15 23:59:59 AND 2012-05-31 23:59:59
-
-    Customer.in_fortnight(2012, 04, field: 'updated_at')
-We'll get the customers updated_at between 2012-04-01 00:00 AND 2012-04-15 23:59
-
-    Customer.in_fortnight(2012, 04, half: :second, field: 'updated_at')
-We'll get the customers updated_at between 2012-04-15 23:59:59 AND 2012-04-30 23:59:59
+## Methods Wiki
+See the wiki page for the methods details
 
--- lib/fortnight.rb
+++ lib/fortnight.rb
@@ -5,24 +5,53 @@ module Fortnight
   def self.included(base)
     base.extend ClassMethods
   end
-  
+
   module ClassMethods
     def in_fortnight(year, month, options={})
 
       begin_of_month = Time.new(year, month)
-      half_of_month = begin_of_month.advance(days: 14).end_of_day
+      half_month = begin_of_month.advance(days: 14).end_of_day
       final_of_month = begin_of_month.end_of_month.end_of_day
 
-      start = options[:half] == :second ? half_of_month : begin_of_month
-      finish = options[:half] == :second ? final_of_month : half_of_month
+      start = options[:half] == :second ? half_month : begin_of_month
+      finish = options[:half] == :second ? final_of_month : half_month
 
       field = options[:field] || 'created_at'
 
       scope = where("#{field} >= :s AND #{field} <= :f", s: start, f: finish)
       scope = scope.order(options[:order]) if options[:order]
+
       scope
     end
+    
+    def last_fortnight
+      today = Date.today
+      date = today - 15.days
+
+      if today.day >= 1 && today.day <= 15
+        self.in_fortnight(date.year, date.month, half: :second)
+      else
+        self.in_fortnight(date.year, date.month)
+      end
+    end
+  end
+
+  def fortnight_in_words
+    date = self.created_at.to_date
+    month_begin = date.beginning_of_month
+    half_month = month_begin.advance(days: 14)
+
+    if date.day >= 1 && date.day <=15
+      output = I18n.l(month_begin, format: :long)
+      output << ' - '
+      output << I18n.l(half_month.to_date, format: :long)
+    else
+      output = I18n.l((half_month + 1.day).to_date, format: :long)
+      output << ' - '
+      output << I18n.l(date.end_of_month, format: :long)
+    end
+
+    output
   end
 end
 
-  
